211a47a8ba3bc1e59a9c69083dc1ed213d1bd0f3
Adina <59285665+lambdina@users.noreply.github.com>
2023-08-22 19:58:03 +0200

    Revert "First draft of our database design"

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.utils.translation import gettext as _
 
-
-class Order(models.Model):
-    email = models.EmailField(
-        _("email address"),
-    )
-    address = models.ForeignKey(
-        "users.Address", blank=True, null=True, on_delete=models.PROTECT
-    )
+# Create your models here.
