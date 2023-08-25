fda114198c4d39671d8a3eed19f2af2835dd260c
Anouar AISSAOUI <anouar.aissaoui@pm.me>
2023-08-25 20:33:30 +0200

    feat(users): setup a custom user model

--- settings/settings.py
+++ settings/settings.py
@@ -129,6 +129,8 @@ REST_FRAMEWORK = {"DEFAULT_PERMISSION_CLASSES": ["services.security.permissions.
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = "apps.users.Client"
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
