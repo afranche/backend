4b198f551f8002f2b4a7a792aaa3dfdecdf8feb3
adina <pirouettecacahuete@protonmail.com>
2023-10-10 09:06:06 +0200

    feat: creates categories

---
+++ apps/listings/management/commands/init_listings.py
@@ -0,0 +1,50 @@
+from django.core.management.base import BaseCommand
+
+from apps.listings.apps import ListingsConfig
+import requests
+import pandas as pd
+import io
+
+from apps.listings.models import Category
+
+
+
+class Command(BaseCommand):
+    def handle(self, *_, **__):
+        if ListingsConfig.objects.count() > 0:
+            print("Listings already initialized.")
+            return
+        url = "https://docs.google.com/spreadsheets/d/1hXSw28QSZwnV31BbuNgGWlxi1kJ1DZd1vHlMshVgTy8/export?format=csv"
+        res = requests.get(url)
+        if res.status_code != 200:
+            print("Failed to fetch listings.", res.content)
+            return
+        df = pd.read_csv(io.BytesIO(res.content), encoding='utf8', sep=",")
+        df = self.clean_df(df)
+
+        self.create_categories(df)
+        print("Categories created.")
+        print(Category.objects.all())
+
+    
+    def create_listings(self, df: pd.DataFrame) -> None:
+        pass
+
+
+    def create_categories(self, df: pd.DataFrame) -> None:
+        df['category'] = df.category.map(lambda x: x.strip())
+        df_cat = df[['category', 'sous catégorie']].drop_duplicates()
+        for name, group in df_cat.groupby(by="category", dropna=False):
+            main_cat = Category(name=name).save()
+            for sub_name in group['sous catégorie'].dropna().unique():
+                if sub_name == "":
+                    continue
+                sub_cat = Category(name=sub_name)
+                sub_cat.parent = main_cat
+                sub_cat.save()
+
+
+
+    def clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.fillna("")
+        return df.drop(['dimensions cmxcmxcm', 'in stock'], axis=1)
