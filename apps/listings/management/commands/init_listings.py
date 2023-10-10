b54155ea9bc3a7126b698d93bad11a4cefb1948b
adina <pirouettecacahuete@protonmail.com>
2023-10-10 12:22:00 +0200

    fix: unit tests

--- apps/listings/management/commands/init_listings.py
+++ apps/listings/management/commands/init_listings.py
@@ -1,17 +1,16 @@
 from django.core.management.base import BaseCommand
-
-from apps.listings.apps import ListingsConfig
+from django.db import models
 import requests
 import pandas as pd
 import io
 
-from apps.listings.models import Category
+from apps.listings.models import Category, Product
 
 
 
 class Command(BaseCommand):
     def handle(self, *_, **__):
-        if ListingsConfig.objects.count() > 0:
+        if Product.objects.count() > 0:
             print("Listings already initialized.")
             return
         url = "https://docs.google.com/spreadsheets/d/1hXSw28QSZwnV31BbuNgGWlxi1kJ1DZd1vHlMshVgTy8/export?format=csv"
@@ -35,15 +34,18 @@ class Command(BaseCommand):
         df['category'] = df.category.map(lambda x: x.strip())
         df_cat = df[['category', 'sous catégorie']].drop_duplicates()
         for name, group in df_cat.groupby(by="category", dropna=False):
-            main_cat = Category(name=name).save()
+            main_cat = Category(name=name)
+            main_cat.save()
             for sub_name in group['sous catégorie'].dropna().unique():
                 if sub_name == "":
                     continue
                 sub_cat = Category(name=sub_name)
                 sub_cat.parent = main_cat
                 sub_cat.save()
+            main_cat.save()
 
-
+                
+            # main_cat.save()
 
     def clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
         df = df.fillna("")
