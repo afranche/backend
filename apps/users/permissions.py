80585d8ddbd0977f785833ae8ff354a407f40fc7
lambdina <pirouettecacahuete@protonmail.com>
2023-11-04 15:01:07 +0100

    wip

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -1,3 +1,5 @@
+import sys
+import warnings
 from rest_framework import permissions
 import logging
 
@@ -11,19 +13,23 @@ class IsAdminOrReadOnly(permissions.IsAdminUser):
 
 class ManageOnlyYourOwn(permissions.BasePermission):
     def has_permission(self, request, view):
+        warnings.warn(f"{request.method}")
+
         if request.method == "POST" or request.user.is_staff:
             # Allow creation (POST) for all or any action for admin users.
             return True
-
-        return False  # Deny all other non-admin users for other actions (GET, PUT, DELETE).
+        if request.method in ["PUT", "PATCH", "DELETE"]:
+            return False  # Deny all other non-admin users for other actions (GET, PUT, DELETE).
 
     def has_object_permission(self, request, view, obj):
+        warnings.warn(f'coucou {request.user} {obj.user}')
         if request.user.is_staff:
             # Admin users have full permissions on the object.
             return True
 
-        if request.method in ["PUT", "PATCH", "DELETE"]:
-            # Normal users can only update or delete their own objects.
-            return obj.user == request.user
 
-        return False  # Deny all other actions for non-admin users.
+            # Normal users can only update or delete their own objects.
+        warnings.warn(f'obj.user == request.user {obj.user == request.user} {obj} {obj.user} {request.user}')
+        print("obj.user == request.user", obj.user == request.user, file=sys.stderr)
+        print(obj, obj.user, request.user, file=sys.stderr)
+        return obj.user == request.user
