7afaee33b240e325df0181ae0f0170e13ec1dbed
Anouar Aissaoui <anouar.aissaoui@pm.me>
2023-11-21 11:57:14 +0100

    feat: add payment and invoice models

--- backend/apps/payment/models.py
+++ backend/apps/payment/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from typing import Any
+
+
+class Invoice(models.Model):
+    order = models.ForeignKey("orders.Order", on_delete=models.PROTECT)
+    file = models.FileField(upload_to="/invoices")
+    date = models.DateField(auto_now_add=True)
+
+    def save(self, *args: Any, **kwargs: Any):
+        if self.id:  # type: ignore
+            raise Exception("Invoice cannot be updated")
+
+        return super().save(*args, **kwargs)
