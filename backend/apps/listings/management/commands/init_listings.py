b58e33a2974aad40299b41f9e13d8d589d632a50
lambdina <pirouettecacahuete@protonmail.com>
2024-01-23 15:47:56 +0100

    oops

--- backend/apps/listings/management/commands/init_listings.py
+++ backend/apps/listings/management/commands/init_listings.py
@@ -11,6 +11,7 @@ import io
 from apps.listings.models import Category, Manufacturer, Product, Listing
 from apps.listings.serializers import ProductSerializer
 from settings.settings import env
+from os import environ
 
 BEEN_THROUGH_BREAKPOINT = False
 
@@ -18,38 +19,14 @@ with open("./.cookies.json", 'r') as f:
     cookies = json.loads(f.read())
 
 def download_image_from_drive(link: str) -> bytes | None:
-    file_id = link.split('/')[-2]
-    import requests
-
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    # 'Accept-Encoding': 'gzip, deflate, br',
-    'Alt-Used': 'drive.usercontent.google.com',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'iframe',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-site',
-    # Requests doesn't support trailers
-    # 'TE': 'trailers',
-    }
-    time.sleep(0.774)
-    response = requests.get(
-        f'https://drive.usercontent.google.com/download?id={file_id}&export=download&authuser=0&confirm=t&uuid=acb6681e-4160-4f9e-8f2a-2cf6988e8542&at=APZUnTV8v6IJON8znqfEl3jjnjR_:1699729320793',
-        cookies=cookies,
-        headers=headers,
-    )
-    warnings.warn(f"Response status code: {response.status_code}")
-    return ContentFile(response.content)
+    return None
 
 class Command(BaseCommand):
     def handle(self, *_, **__):
         if Product.objects.count() > 0:
             print("Listings already initialized.")
             return
-        url = "https://docs.google.com/spreadsheets/d/1hXSw28QSZwnV31BbuNgGWlxi1kJ1DZd1vHlMshVgTy8/export?format=csv"
+        url = environ.get("DRIVE_CSV_URL")
         res = requests.get(url)
         if res.status_code != 200:
             print("Failed to fetch listings.", res.content)
